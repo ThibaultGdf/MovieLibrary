e166841565e70d1a117e60561e02594652512af9
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2024-03-07 10:23:03 +0100

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Thibault GODEFROY on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
